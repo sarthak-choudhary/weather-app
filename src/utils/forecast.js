b99205197af1fe325da51c55cac5f63e51f154b3
sarthak-choudhary <f20190112@pilani.bits-pilani.ac.in>
2020-04-28 17:29:49 +0530

    Add high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + `It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`)
-
+            callback(undefined, body.daily.data[0].summary + `It is currently ${body.currently.temperature} degrees out.\nThe high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}.\nThere is a ${body.currently.precipProbability}% chance of rain`)
+            
         }
     })
 }
